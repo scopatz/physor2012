c46da9f767e9e68b28c6f28873c099c9006c4973
Anthony Scopatz <scopatz@gmail.com>
2011-11-12 11:24:39 -0600

    Refactored anaylsis routines.

--- analysis/diffs.py
+++ analysis/diffs.py
@@ -5,15 +5,18 @@ import tables as tb
 
 from nuc_sets import serp_nucs, model_nucs 
 
-nuclides = set(['U234',  'U235',  'U236',  'U238',  'NP237', 'PU238', 'PU239', 
+NUCLIDES = set(['U234',  'U235',  'U236',  'U238',  'NP237', 'PU238', 'PU239', 
                 'PU240', 'PU241', 'PU242', 'AM241', 'AM242', 'AM243', 'CM242', 
                 'CM243', 'CM244', 'CM245', 'CM246', 'SE79',  'KR85',  'SR90',  
                 'ZR93',  'TC99',  'I129',  'PD107', 'CS134', 'CS135', 'CS137', 
                 'SM151', 'EU155', 'H1',    'H3',    'O16',   'HE4',   'NA23',])
 
 # Ensure only serpent nuclides
-nuclides = (nuclides & serp_nucs)
-nuclides = serp_nucs
+NUCLIDES = (NUCLIDES & serp_nucs)
+NUCLIDES = serp_nucs
+
+BASEPATH = '../data/lwr_base.h5'
+PHYSPATH = '../data/lwr_physor2012.h5'
 
 
 def load_sigma_s(nucs, lib, p):
@@ -68,7 +71,7 @@ def sort_frac_diff(fdiff):
 
 
 
-def main():
+def analyze_vs_lib(r, libpath, p_start, p_offset):
     r0 = 0.369
     r025 = 0.3895
     r05 = 0.41
@@ -78,40 +81,40 @@ def main():
     p_offset = 0
 
     # r_fuel = 0.9 base
-    s_g0, s_gh0 = load_sigma_s(nuclides, '/home/scopatz/lwr_physor2012.h5', 0 + p_offset)
+    s_g0, s_gh0 = load_sigma_s(NUCLIDES, PHYSPATH, 0 + p_offset)
     gtp0 = group_transfer_prob(s_g0, s_gh0)
 
     # r_fuel = 1.1 base
-    s_g1, s_gh1 = load_sigma_s(nuclides, '/home/scopatz/lwr_physor2012.h5', 5 + p_offset)
+    s_g1, s_gh1 = load_sigma_s(NUCLIDES, PHYSPATH, 5 + p_offset)
     gtp1 = group_transfer_prob(s_g1, s_gh1)
 
-    # r_fuel = 0.95 base
-    s_g025, s_gh025 = load_sigma_s(nuclides, '/home/scopatz/lwr_physor2012.h5', 10 + p_offset)
-    gtp025 = group_transfer_prob(s_g025, s_gh025)
+    # r_fuel from lib
+    s_g, s_gh = load_sigma_s(NUCLIDES, libpath, p_start + p_offset)
+    gtp = group_transfer_prob(s_g, s_gh)
 
-    # Interpolate to 0.95 base
-    is_g025, is_gh025, igtp025 = interp_xs(r025, r0, r1, s_g0, s_g1, s_gh0, s_gh1)
-    fdiff025 = frac_diff(is_gh025, s_gh025, s_g025)
-    sfdiff025 = sort_frac_diff(fdiff025)
-    #for nuc, ind, val in sfdiff025:
-    #    if (s_gh0[nuc].flat[ind] != 0.0)   and (s_gh1[nuc].flat[ind] != 0.0) and \
-    #       (s_gh025[nuc].flat[ind] != 0.0) and (is_gh025[nuc].flat[ind] != 0.0):
-    #        print nuc, val, (ind/19, ind%19), s_gh025[nuc].flat[ind]
+    # Interpolated values
+    is_g, is_gh, igtp = interp_xs(r, r0, r1, s_g0, s_g1, s_gh0, s_gh1)
+    fdiff = frac_diff(is_gh, s_gh, s_g)
+    sfdiff = sort_frac_diff(fdiff)
+    for nuc, ind, val in sfdiff:
+        if (s_gh[nuc].flat[ind] != 0.0)  and (is_gh[nuc].flat[ind] != 0.0) and \
+           (s_gh0[nuc].flat[ind] != 0.0) and (s_gh1[nuc].flat[ind] != 0.0):
+            print nuc, val, (ind/19, ind%19), s_gh[nuc].flat[ind]
 
 
-    # r_fuel = 1.05 base
-    s_g075, s_gh075 = load_sigma_s(nuclides, '/home/scopatz/lwr_physor2012.h5', 15 + p_offset)
-    gtp075 = group_transfer_prob(s_g075, s_gh075)
+def main():
+    r0 = 0.369
+    r025 = 0.3895
+    r05 = 0.41
+    r075 = 0.4305
+    r1 = 0.451
 
-    # Interpolate to 1.05 base
-    is_g075, is_gh075, igtp075 = interp_xs(r075, r0, r1, s_g0, s_g1, s_gh0, s_gh1)
-    fdiff075 = frac_diff(is_gh075, s_gh075, s_g075)
-    sfdiff075 = sort_frac_diff(fdiff075)
-    for nuc, ind, val in sfdiff075:
-        if (s_gh0[nuc].flat[ind] != 0.0)   and (s_gh1[nuc].flat[ind] != 0.0) and \
-           (s_gh075[nuc].flat[ind] != 0.0) and (is_gh075[nuc].flat[ind] != 0.0):
-            print nuc, val, (ind/19, ind%19), s_gh075[nuc].flat[ind]
+    p_offset = 0
 
+    analyze_vs_lib(r025, PHYSPATH, 10, p_offset)
+    analyze_vs_lib(r05,  BASEPATH, 0,  p_offset)
+    analyze_vs_lib(r075, PHYSPATH, 15, p_offset)
+    
 
 if __name__ == '__main__':
     main()
